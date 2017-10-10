1511edd6d9d5529a56bc6f3316b66aebb613c8e1
nscharrenberg <n.scharrenberg@student.fontys.nl>
2017-10-10 17:03:12 +0200

    AEX Banner RMI Puller Init
    Adding AEX Banner RMI Puller Asignment - Clone of the Local AEX Banner.

---
+++ AEXBanner RMI Puller/src/aexbanner/local/Fund.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aexbanner.local;
+
+/**
+ *
+ * @author Noah Scharrenberg
+ */
+public class Fund implements IFunds {
+    private String name;
+    private double rate;
+    
+    public Fund(String name, double rate) {
+        this.name = name;
+        this.rate = rate;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getRate() {
+        return rate;
+    }
+    
+    public void setRate(double value) {
+        if (rate + value < 0) {
+            rate += (value * -1);
+        } else {
+            rate += value;
+        }
+    }
+}
